2b795ff27e75c4d6984c31e7964ee77364a0f7b9
Timea Toth <tiemka14@gmail.com>
2025-06-22 20:03:17 +0000

    publish gradio app

--- app/app.py
+++ app/app.py
@@ -88,4 +88,4 @@ def build_interface():
 
 if __name__ == "__main__":
     demo = build_interface()
-    demo.launch(server_name="0.0.0.0", server_port=7860, share=False) 
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=True) 
